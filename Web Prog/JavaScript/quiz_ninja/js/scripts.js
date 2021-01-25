9828e5a7e1553d1318766d5f422d60c47e38ddc3
Federico Turban <fturbana@hotmail.com>
2021-01-24 21:19:12 -0600

    Added Comment

--- Web Prog/JavaScript/quiz_ninja/js/scripts.js
+++ Web Prog/JavaScript/quiz_ninja/js/scripts.js
@@ -1,7 +1,7 @@
 // welcome the user
 alert("Welcome to Quiz Ninja!");
 
-
+// Comment
 // The first part contains the question, the second part contains the answer of such question
 var quiz = [
 	["What is Fede's full name?","Federico Guillermo Turban Almeida"],
