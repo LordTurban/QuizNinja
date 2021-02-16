5e93d7d5ed45d75fad2fa8e45e25be2e6968a91d
Federico Turban <fturbana@hotmail.com>
2021-02-15 23:24:20 -0600

    Made some changes in the scrip.js file for the quiz_ninja project

--- Web Prog/JavaScript/quiz_ninja/js/scripts.js
+++ Web Prog/JavaScript/quiz_ninja/js/scripts.js
@@ -1,26 +1,22 @@
 // welcome the user
 alert("Welcome to Quiz Ninja!");
 
-// Check the Name Change
 // The first part contains the question, the second part contains the answer of such question
 var quiz = [
-	["What is Fede's full name?","Federico Guillermo Turban Almeida"],
-	["What is Tlahui's full name?","Tlahuiltzin Shurabi Raygoza Valle"],
-	["What is Bolsas' full name?","Hector Rurawe Sanchez Avelar"],
-	["What is Pancho's full name?","Francisco Javier Aceves Lopez"],
-	["What is Testicle's full name?","Manuel Isaias Garcia Castro"],
-	["What is C4G stands for?","Carry 4 Gold"],
+	["What is Superman's real Name?", "Clark Kent"],
+	["What is Wonder Woman's real Name?", "Dianna Prince"],
+	["What is Batman's real Name?", "Bruce Wayne"],
 	];
 
 var score = 0 // initialize score
 
-for(var i=0,max=quiz.length;i<max;i++){
+for (var i = 0, max = quiz.length; i < max; i++) {
 
 	// get answer from user
 	var answer = prompt(quiz[i][0]); // quiz[i][0] is the ith question
 
 	//check if answer is correct
-	if(answer === quiz[i][1]){ // quiz[i][1] is the ith answer
+	if (answer === quiz[i][1]){ // quiz[i][1] is the ith answer
 		alert("Correct!");
 		// increase score by 1
 		score++;
